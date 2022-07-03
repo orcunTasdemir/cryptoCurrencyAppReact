6bf9837314d223b5993b3dfdc7e8e829aed7c857
orcunTasdemir <orcntasdemir@gmail.com>
2022-07-03 02:39:31 -0500

    First commit with coins and news working

---
+++ src/components/News.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const demoImage =
+  "https://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
+
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 6 : 12,
+  });
+  if (!cryptoNews?.value) return "Loading ...";
+
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews?.value?.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  alt="The news article visual"
+                />
+              </div>
+              <p>Continue</p>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default News;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "3eba6c575emshb346b6b1cc41417p10b21bjsn1db275605d43",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) =>
+        createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+    }),
+
+    // Note: To access this endpoint you need premium plan
+    getExchanges: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
